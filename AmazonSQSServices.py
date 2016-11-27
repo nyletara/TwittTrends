2c63effa8604a7643cb51a6d1fc28b383cb99a99
nyletara <iguru95@gmail.com>
2016-11-26 22:44:58 -0500

    Added send + receieve for SQS

---
+++ AmazonSQSServices.py
@@ -0,0 +1,21 @@
+import boto3
+
+class SQSServices():
+	
+	"""docstring for SQSServices"""
+
+	def __init__(self):
+		self.sqs = boto3.resource('sqs')
+
+	def getQueueName(self, name):
+		queueName = self.sqs.get_queue_by_name(QueueName=name)
+		return queueName
+
+	def sendMessage(queueName, message):
+		return queueName.send_messages(MessageBody=message)
+
+	def receiveMessage(queueName, messageAttribute):
+		return queueName.receive_messages(MessageAttributeNames=[messageAttribute])
+
+		
+		
